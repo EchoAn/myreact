57a5a18da45998d9896e477f46f48b454dbb0c48
echo <anjin@new4g.cn>
2017-04-07 10:13:03 +0800

    update

--- webpacks/webpack.config.dev.babel.js
+++ webpacks/webpack.config.dev.babel.js
@@ -1,10 +1,10 @@
 import {
-    resolve
+    resolve,
 } from 'path';
 import webpack from 'webpack';
 import objectAssignDeep from 'object-assign-deep';
 
-import webpackCommonConfig from './webpack.config.common.babel.js';
+import webpackCommonConfig from './webpack.config.common.babel';
 
 const webpackDevConfig = {
     entry: {
@@ -18,7 +18,8 @@ const webpackDevConfig = {
             // 为热替换（HMR）打包好运行代码
             // only- 意味着只有成功更新运行代码才会执行热替换（HMR
             'webpack/hot/only-dev-server',
-            './src/index.js'
+            'babel-polyfill',
+            './src/index.js',
         ],
     },
     output: {
@@ -27,7 +28,7 @@ const webpackDevConfig = {
         path: resolve(__dirname, 'dist'),
 
         // 对于热替换（HMR）是必须的，让webpack知道在哪里载入热更新的模块（chunk）
-        publicPath: '/'
+        publicPath: '/',
     },
 
     devServer: {
@@ -38,11 +39,11 @@ const webpackDevConfig = {
         // 和上文output的"publicPath"值保持一致
         publicPath: '/',
 
-        //启动gzip压缩
+        // 启动gzip压缩
         compress: true,
 
-        //当使用HTML5 History API，任意的 404 响应可以提供为 index.html 页面。通过传入以下启用
-        //单页应用请求404时会定位到index.html
+        // 当使用HTML5 History API，任意的 404 响应可以提供为 index.html 页面。通过传入以下启用
+        // 单页应用请求404时会定位到index.html
         historyApiFallback: true,
         noInfo: true,
     },
@@ -50,11 +51,11 @@ const webpackDevConfig = {
     module: {
         rules: [{
             test: /\.(jpe?g|png|gif)$/i,
-            use: ['url-loader?limit=8000&name=images/[name].[ext]']
+            use: ['url-loader?limit=8000&name=images/[name].[ext]'],
         }, {
             test: /\.(svg|woff|woff2|ttf|eot)$/i,
-            use: ['file-loader?name=font/[name].[ext]']
-        }]
+            use: ['file-loader?name=font/[name].[ext]'],
+        }],
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
